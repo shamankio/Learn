cbdae68da7b3da72c5ad48b15c04959b4319fff5
Sasha R <rustanovych@gmail.com>
2014-10-04 17:33:35 +0300

    //stading book

---
+++ src/book/chap27/CDLDemo.java
@@ -0,0 +1,39 @@
+package book.chap27;
+// An example of CountDownLatch.
+//dem
+
+import java.util.concurrent.CountDownLatch;
+
+class CDLDemo {
+    public static void main(String args[]) {
+        CountDownLatch cdl = new CountDownLatch(5);
+
+        System.out.println("Starting");
+
+        new MyThread(cdl);
+
+        try {
+            cdl.await();
+        } catch (InterruptedException exc) {
+            System.out.println(exc);
+        }
+
+        System.out.println("Done");
+    }
+}
+
+class MyThread implements Runnable {
+    CountDownLatch latch;
+
+    MyThread(CountDownLatch c) {
+        latch = c;
+        new Thread(this).start();
+    }
+
+    public void run() {
+        for(int i = 0; i<5; i++) {
+            System.out.println(i);
+            latch.countDown(); // decrement count
+        }
+    }
+}
